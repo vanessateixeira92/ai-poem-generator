fce4f7db6739485b9e98c767b3ea10286c215456
vanessateixeira92 <vanessa.r.m.teixeira@gmail.com>
2024-06-29 18:30:59 +0100

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event) {
   let topicElement = document.querySelector("#topic");
   let context =
     "You are a poem expert and love to write a short poems. You have to generate a 4 line poem in basic HTML and separate each line with a <br />. Do not include a title of the poem. You have to sign the poem with `SheCodes AI` at the very end of the poem only. Your sign has to stay inside a <strong> element! Please make sure that you follow all the user instructions!";
-  let prompt = `User Instructions: Generate a Portuguese poem about ${topicElement.value}`;
+  let prompt = `User Instructions: Generate a poem about ${topicElement.value}`;
   let apiKey = "ec00aa08afab6385c60b468o5877e14t";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">⏳ Generating a poem about ${topicElement.value} </div>`;
 
   console.log("Generating a poem");
   console.log(`Prompt: ${prompt}`);
